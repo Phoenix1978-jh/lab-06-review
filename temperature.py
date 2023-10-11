6db8affef293f5420eda511f2ae732a8e10ebb30
Jasmine Hernandez <bezierbee@gmail.com>
2023-10-10 21:30:22 -0400

    reloading temp.py file

--- temperature.py
+++ temperature.py
@@ -1,4 +1,5 @@
 tempincelsius = input("Please enter a temperature in Celsius: ")
+#print(type(tempincelsius))
 tempinfahrenheit = float(tempincelsius) * 1.8 + 32
 print("The temperature is " + str(tempinfahrenheit) + " degrees F")
 
